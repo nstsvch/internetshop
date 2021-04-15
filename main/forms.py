ac54d345743f7c4185a4df816ac23ae3fabb1d97
Anastasiya <a5rs5a8r8gmail.com>
2021-04-15 22:38:05 +0300

    Создание формы - регистрация

---
+++ main/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+
+class RegistrationForm(UserCreationForm):
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    phone = forms.CharField(required=True, label='Телефон', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True, label='Почта', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone', 'password']
